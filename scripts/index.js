e506bfd4956c8decff921ae4f48a8bb58dd0c737
fvour <laurance.sebastian@gmail.com>
2024-05-15 23:07:53 +0700

    Update direction for page (home to login and login to signup)

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
-function login(name) {
-    let loginButton = document.getElementById("login-button");
-    let profileContainer = document.getElementById("header-profile-container")
-    let profileNameText = document.getElementById("profile-name")
+// function login(name) {
+//     let loginButton = document.getElementById("login-button");
+//     let profileContainer = document.getElementById("header-profile-container")
+//     let profileNameText = document.getElementById("profile-name")
 
 
-    loginButton.style.display = "none";
-    profileContainer.style.display = "flex";
-    profileNameText.innerText = name;
-}
+//     loginButton.style.display = "none";
+//     profileContainer.style.display = "flex";
+//     profileNameText.innerText = name;
+// }
 
-document.addEventListener("DOMContentLoaded", function(){
-    let loginButton = document.getElementById("login-button");
-    loginButton.onclick = function(){
-        let name = prompt("Input username:");
-        login(name);
-    }
-})
+// document.addEventListener("DOMContentLoaded", function(){
+//     let loginButton = document.getElementById("login-button");
+//     loginButton.onclick = function(){
+//         let name = prompt("Input username:");
+//         login(name);
+//     }
+// })
