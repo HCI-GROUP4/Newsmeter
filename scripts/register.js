d74a1c80407cc09c985bd60ece09c26a38340179
Sergie211105 <sergie211105@gmail.com>
2024-06-04 14:45:11 +0700

    update

--- scripts/register.js
+++ scripts/register.js
@@ -1,25 +1,23 @@
 function validateForm(event) {
-    event.preventDefault(); // Prevent form submission
     
     // Get form values
-    let firstName = document.querySelector('.search-input-fname').value;
-    let lastName = document.querySelector('.search-input-lname').value;
+    let firstName = document.querySelector('#search-input-fname').value;
+    let lastName = document.querySelector('#search-input-lname').value;
     let day = document.querySelector('#dob-day').value;
     let month = document.querySelector('#dob-month').value;
     let year = document.querySelector('#dob-year').value;
     let email = document.querySelector('.search-input-email').value;
-    let password = document.querySelector('.search-input-password').value;
-    let confirmPassword = document.querySelector('.search-input-cpass').value;
+    let password = document.querySelector('#search-input-password').value;
+    let confirmPassword = document.querySelector('#search-input-cpass').value;
 
     // Check if all fields are filled
     if (firstName && lastName && day && month && year && email && password && confirmPassword) {
         // Check if passwords match
         if (password === confirmPassword) {
-            window.location.href = 'login.html'; // Redirect to login page
+            alert('Register successful!!!');
         } else {
             alert('Passwords do not match.');
+            event.preventDefault(); // Prevent form submission  
         }
-    } else {
-        alert('Please fill out all fields.');
     }
 }
